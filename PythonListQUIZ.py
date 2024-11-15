c74d94ab69fa50495bfd42285776169c30eeb58b
watermelon-cat <watermeloncat210@gmail.com>
2024-11-15 13:55:48 -0700

    Create PythonListQUIZ.py

---
+++ PythonListQUIZ.py
@@ -0,0 +1,19 @@
+#1, make list and print it
+zoo = ["monkey", "elephant", "lion", "tiger", "giraffe"]
+print(zoo)
+
+#add panda to end of list
+zoo.append("panda")
+print("List after additions: ", zoo)
+
+#print the number of animals
+number_of_animals = len(zoo)
+print("we now have", number_of_animals, "animals")
+
+#delete the lion from the list
+zoo.remove("lion")
+print("After the lion escaped these are the animals in the zoo:", zoo)
+
+#replace elephant with meerkat and print the list
+zoo[1] = "meerkat"
+print(zoo)
